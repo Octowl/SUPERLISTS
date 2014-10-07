e19e7f036a4ed745bf880ac42b7da2de8c9846c9
Octowl <alsaff1987@gmail.com>
2014-10-07 14:21:31 +0300

    Refactored function tests to get rid of bad code smell

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app. She goes
@@ -38,13 +43,8 @@ class NewVisitorTest(LiveServerTestCase):
         # to-do list table
         input_box.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/+')
-        self.check_row_in_table('1: Buy peacock feathers')
-
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -54,8 +54,8 @@ class NewVisitorTest(LiveServerTestCase):
 
 
         # The page updates again, and now shows both items on her list
-        self.check_row_in_table('1: Buy peacock feathers')
-        self.check_row_in_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # A new user, Francis, comes along to the site
 
