c6d93258693144617bc8e457a980b2dc2049ff54
Octowl <alsaff1987@gmail.com>
2014-11-08 10:11:45 +0300

    swtiched the view for a new list to a CBGV

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView
 
 from lists.models import Item, List
 from lists.forms import ExistingListItemForm, ItemForm
@@ -8,6 +8,14 @@ from lists.forms import ExistingListItemForm, ItemForm
 class HomePageView(FormView):
     template_name = 'home.html'
     form_class = ItemForm
+
+
+class NewListView(CreateView, HomePageView):
+
+    def form_valid(self, form):
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
 
 
 def view_list(request, list_id):
@@ -19,13 +27,3 @@ def view_list(request, list_id):
             form.save()
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, 'form': form})
-
-
-def new_list(request):
-    form = ItemForm(data=request.POST)
-    if form.is_valid():
-        list_ = List.objects.create()
-        form.save(for_list=list_)
-        return redirect(list_)
-    else:
-        return render(request, 'home.html', {'form': form})
